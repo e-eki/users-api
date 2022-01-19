029f859da1ec1e73e1f94aadf27870ed31d9e658
vdremina <vdremina@phoenixit.ru>
2022-01-19 12:57:01 +0300

    fix config

--- server/config.js
+++ server/config.js
@@ -1,27 +1,20 @@
 'use strict';
 
-// const NODE_ENV = process.env.NODE_ENV || 'development';
-
 module.exports = {
     version: '1.0'
     
     // настройки сервера
     , server: {
-        port: 3000   //process.env.PORT || 3000
+        port: 3000
         , host: 'localhost'
         , protocol: 'http'
     }
-    // , server: {    // для heroku
-    //     port: process.env.PORT || 8000
-    //     , host: 'test-task-users-app.herokuapp.com'
-    //     , protocol: 'https'
-    // }
     // название файла с логами БД
     , logFileName: 'dbLogs.log'
     // настройки соединения с БД
     , db : {
         mongo : {
-            url: 'mongodb+srv://eeki:feanoring1320@cluster0.tltda.mongodb.net/UsersAppDatabase?retryWrites=true&w=majority'
+            url: '<url>'
             , options: {
                 autoReconnect: true
                 , useNewUrlParser: true 
